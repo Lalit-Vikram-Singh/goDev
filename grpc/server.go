442f9772f9391e519ae5cd4f3bf11a910503cddd
Lalit-Vikram-Singh <lalit@opsmx.io>
2023-10-31 15:53:33 +0530

    grpc example

---
+++ grpc/server.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+
+	pb "grpc/pb"
+	"google.golang.org/grpc"
+
+)
+
+type server struct {
+	pb.GreetingServiceServer
+}
+
+func (s *server) Greeting(ctx context.Context, req *pb.GreetingServiceRequest) (*pb.GreetingServiceReply, error) {
+	return &pb.GreetingServiceReply{
+		Message: fmt.Sprintf("Hello, %s", req.Name),
+	}, nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		panic(err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterGreetingServiceServer(s, &server{})
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+
+}
